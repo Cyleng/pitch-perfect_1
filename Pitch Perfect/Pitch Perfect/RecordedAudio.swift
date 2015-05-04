14c5ac7f520891dc2662645f32167e002aa4c243
Chuanyu Leng <jacques.leng@gmail.com>
2015-05-04 20:18:51 +0800

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Chuan Yu Leng on 4/28/15.
+//  Copyright (c) 2015 MatchVZ. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio {
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
